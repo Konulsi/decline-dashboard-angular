48e1c494965a853a0ecb2725ed03eb3821c0908d
Konul.Ibrahimova.S <Konul.Ibrahimova.S@unibank.az>
2024-05-08 11:24:43 +0400

    .

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -40,42 +40,16 @@ export class HeaderComponent {
       );
   }
 
-  // createExcelFile(data: IDeclineList[] | IDeclineList) {
-  //   let dataArray: any[];
-
-  //   if (Array.isArray(data)) {
-  //     dataArray = data.map((item) => [
-  //       item.key,
-  //       item.type,
-  //       item.count,
-  //       item.number,
-  //     ]);
-  //   } else if (data) {
-  //     dataArray = [[data.key, data.type, data.count, data.number]];
-  //   } else {
-  //     console.error('No data received from API.');
-  //     return;
-  //   }
-
-  //   const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(dataArray);
-  //   const wb: XLSX.WorkBook = XLSX.utils.book_new();
-  //   XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-  //   XLSX.writeFile(wb, 'data.xlsx');
-  //   console.log('Clicked on Excel button');
-  // }
-
   createExcelFile(data: any) {
-    // API yanıtını uygun bir dizi formatına dönüştür
     const jsonData = data.content.map((item: any) => {
       return {
-        property1: item.key,
-        property2: item.type,
-        property3: item.number,
-        property4: item.count,
+        ID: item.key,
+        Name: item.type,
+        Number: item.number,
+        Count: item.count,
       };
     });
 
-    // JSON verisini Excel sayfasına dönüştür
     const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(jsonData);
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
